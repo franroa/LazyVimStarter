bc27e2f03e1496942c87a95aa503142dcd36096e
Francisco Roa Prieto <francisco.roa.prieto@synaos.com>
2023-07-03 10:52:44 +0200

    [main]: more
    Why:
    
    How:
    
    Tags:
    
    Author:
    Francisco Roa Prieto
    Branch:
    main
    Jira Ticket:
    main

--- lua/plugins/extend_luasnip.lua
+++ lua/plugins/extend_luasnip.lua
@@ -36,6 +36,37 @@ return {
       --     return item
       --   end,
       -- }
+      opts.sorting = {
+        -- TODO: https://www.reddit.com/r/neovim/comments/14k7pbc/what_is_the_nvimcmp_comparatorsorting_you_are/
+        -- TODO: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
+        comparators = {
+          cmp.config.compare.offset,
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+
+          -- copied from cmp-under, but I don't think I need the plugin for this.
+          -- I might add some more of my own.
+          function(entry1, entry2)
+            local _, entry1_under = entry1.completion_item.label:find "^_+"
+            local _, entry2_under = entry2.completion_item.label:find "^_+"
+            entry1_under = entry1_under or 0
+            entry2_under = entry2_under or 0
+            if entry1_under > entry2_under then
+              return false
+            elseif entry1_under < entry2_under then
+              return true
+            end
+          end,
+
+          -- cmp.config.compare.recently_used,
+          -- cmp.config.compare.locality,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
+      }
+
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources,
         { { name = "emoji" }, { name = "cmdline" }, { name = "codeium" } }))
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
